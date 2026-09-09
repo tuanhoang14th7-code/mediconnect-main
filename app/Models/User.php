7070634a5e743cf56ce5dafc63dc15870335b7fe
TuanHoang <tuanhoang14th7@gmail.com>
2026-09-09 21:14:26 +0700

    link users to cities

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,7 +58,7 @@ class User extends Authenticatable
     
     public function doctorDetails()
     {
-        return $this->hasOne(Doctor::class, 'user_id');
+         return $this->hasOne(Doctor::class, 'user_id');
     }
     public function appointments()
     {
